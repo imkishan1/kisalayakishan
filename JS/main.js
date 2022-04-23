029306264afd1f3c1fff6ec920eb3d11648d6783
Kisalaya Kishan <kislay.kishan007@gmail.com>
2022-04-23 16:56:38 +0530

    update

--- JS/main.js
+++ JS/main.js
@@ -66,5 +66,20 @@ const handleSubmit = (event) => {
 }
 
 
+// document.querySelector("form").addEventListener("submit", handleSubmit);
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+//   let myForm = document.getElementById("");
+//   let formData = new FormData(myForm);
+//   fetch("/", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//     body: new URLSearchParams(formData).toString(),
+//   })
+//     .then(() => console.log("Form successfully submitted"))
+//     .catch((error) => alert(error));
+// };
+
 
 
